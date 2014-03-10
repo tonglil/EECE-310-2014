937e72a19581da42d3cd54fb92691c582f197be2
Tony Li <tonyli.lives@gmail.com>
2014-03-10 13:36:03 -0700

    UndoableGame class

---
+++ src/main/java/ca/ubc/jpacman/framework/model/UndoableGame.java
@@ -0,0 +1,21 @@
+package ca.ubc.jpacman.framework.model;
+
+import org.jpacman.framework.model.Game;
+import org.jpacman.framework.model.Direction;
+import org.jpacman.framework.model.Ghost;
+
+public class UndoableGame extends Game {
+    public void undo() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void movePlayer(Direction dir) {
+        super.movePlayer(dir);
+    }
+
+    @Override
+    public void moveGhost(Ghost theGhost, Direction dir) {
+        super.moveGhost(theGhost, dir);
+    }
+}
