078f59d1d5ee239011e7bc5e935f86b88d5c8d35
scolin22 <colin@bunnylaundering.com>
2014-03-11 03:59:56 -0700

    added test cases

--- src/test/java/ca/ubc/jpacman/test/framework/accept/UndoStoryTest.java
+++ src/test/java/ca/ubc/jpacman/test/framework/accept/UndoStoryTest.java
@@ -1,19 +1,22 @@
 package ca.ubc.jpacman.test.framework.accept;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.jpacman.framework.factory.FactoryException;
 import org.jpacman.framework.model.Direction;
+import org.jpacman.framework.model.Tile;
+import org.junit.Test;
 
 import ca.ubc.jpacman.framework.ui.UndoablePacman;
 import ca.ubc.jpacman.test.framework.ui.MovePlayerStoryTest;
-import org.jpacman.framework.factory.FactoryException;
-import org.jpacman.framework.model.Tile;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class UndoStoryTest extends MovePlayerStoryTest {
     private UndoablePacman pacman;
 
     @Override
-    public UndoablePacman makeUI(){
+    public UndoablePacman makeUI() {
         pacman = new UndoablePacman();
         try {
             return (UndoablePacman) pacman.initialize();
@@ -23,14 +26,14 @@ public class UndoStoryTest extends MovePlayerStoryTest {
     }
 
     @Override
-    protected UndoablePacman getUI(){
+    protected UndoablePacman getUI() {
         return pacman;
     }
 
     @Test
-    public void test_S7_1_UndoPlayerLeft(){
-        Tile PlayerTile = tileAt(1,1);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_1_UndoPlayerLeft() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
         getEngine().start();
         getEngine().left();
         assertTrue(getPlayer().getPoints() > 0);
@@ -39,15 +42,45 @@ public class UndoStoryTest extends MovePlayerStoryTest {
         assertEquals(PlayerTile, getPlayer().getTile());
         assertEquals(0, getPlayer().getPoints());
         assertEquals(GhostTile, theGhost().getTile());
-
     }
 
     @Test
-    public void test_S7_2_UndoPlayerRight(){
-        Tile PlayerTile = tileAt(1,1);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_1_UndoPlayerLeftOnly() {
+        Tile PlayerTile = tileAt(1, 1);
         getEngine().start();
-        //Make sure Player doesn't die when moved right, so move ghost first
+        getEngine().left();
+        getUI().undo();
+        assertEquals(PlayerTile, getPlayer().getTile());
+    }
+
+    @Test
+    public void test_S7_1_UndoGhostLeftOnly() {
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        getEngine().left();
+        getUI().getGame().moveGhost(theGhost(), Direction.LEFT);
+        getUI().undo();
+        assertEquals(GhostTile, theGhost().getTile());
+    }
+
+    @Test
+    public void test_S7_1_UndoPlayerPoints() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        getEngine().left();
+        assertTrue(getPlayer().getPoints() > 0);
+        getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
+        getUI().undo();
+        assertEquals(0, getPlayer().getPoints());
+    }
+
+    @Test
+    public void test_S7_2_UndoPlayerRight() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        // Make sure Player doesn't die when moved right, so move ghost first
         getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
         getEngine().right();
         getUI().undo();
@@ -57,12 +90,37 @@ public class UndoStoryTest extends MovePlayerStoryTest {
     }
 
     @Test
-    public void test_S7_3_UndoPlayerNoMovement(){
-        Tile PlayerTile = tileAt(1,1);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_2_UndoPlayerRightOnly() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        // Make sure Player doesn't die when moved right, so move ghost first
+        getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
+        getEngine().right();
+        getUI().undo();
+        assertEquals(PlayerTile, getPlayer().getTile());
+    }
+
+    @Test
+    public void test_S7_2_UndoGhostRightOnly() {
+        Tile PlayerTile = tileAt(1, 1);
+        getEngine().start();
+        getEngine().left();
+        // Make sure Player doesn't die when moved right, so move ghost first
+        getUI().getGame().moveGhost(theGhost(), Direction.LEFT);
+        getUI().getGame().moveGhost(theGhost(), Direction.RIGHT);
+        getUI().undo();
+        assertEquals(PlayerTile, theGhost().getTile());
+    }
+
+    @Test
+    public void test_S7_3_UndoPlayerNoMovement() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
         getEngine().start();
         getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
-        //Moving down from initial state does not move the player(against the wall), so it counts as no movement
+        // Moving down from initial state does not move the player(against the wall), so it counts
+        // as no movement
         getEngine().down();
         getUI().undo();
         assertEquals(PlayerTile, getPlayer().getTile());
@@ -71,15 +129,26 @@ public class UndoStoryTest extends MovePlayerStoryTest {
     }
 
     @Test
-    public void test_S7_4_UndoPlayerUp(){
-        Tile PlayerTile = tileAt(1,1);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_3_UndoPlayerNoMovementOnly() {
+        Tile PlayerTile = tileAt(1, 1);
         getEngine().start();
-        //Player moves up
+        // Moving down from initial state does not move the player(against the wall), so it counts
+        // as no movement
+        getEngine().down();
+        getUI().undo();
+        assertEquals(PlayerTile, getPlayer().getTile());
+    }
+
+    @Test
+    public void test_S7_4_UndoPlayerUp() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        // Player moves up
         getEngine().up();
-        //Since no points here, moving up won't increment points
-        assertEquals(0,getPlayer().getPoints());
-        //Ghost moves down
+        // Since no points here, moving up won't increment points
+        assertEquals(0, getPlayer().getPoints());
+        // Ghost moves down
         getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
         getUI().undo();
         assertEquals(PlayerTile, getPlayer().getTile());
@@ -88,9 +157,29 @@ public class UndoStoryTest extends MovePlayerStoryTest {
     }
 
     @Test
-    public void test_S7_5_UndoPlayerDown(){
-        Tile Point1Tile = tileAt(0,2);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_4_UndoPlayerUpOnly() {
+        Tile PlayerTile = tileAt(1, 1);
+        getEngine().start();
+        // Player moves up
+        getEngine().up();
+        getUI().undo();
+        assertEquals(PlayerTile, getPlayer().getTile());
+    }
+
+    @Test
+    public void test_S7_4_UndoGhostUpOnly() {
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        // Ghost moves up
+        getUI().getGame().moveGhost(theGhost(), Direction.UP);
+        getUI().undo();
+        assertEquals(GhostTile, theGhost().getTile());
+    }
+
+    @Test
+    public void test_S7_5_UndoPlayerDown() {
+        Tile Point1Tile = tileAt(0, 2);
+        Tile GhostTile = tileAt(2, 1);
         getEngine().start();
         getUI().getGame().moveGhost(theGhost(), Direction.UP);
         getUI().getGame().moveGhost(theGhost(), Direction.LEFT);
@@ -100,13 +189,35 @@ public class UndoStoryTest extends MovePlayerStoryTest {
         assertEquals(GhostTile, getPlayer().getTile());
         assertEquals(0, getPlayer().getPoints());
         assertEquals(Point1Tile, theGhost().getTile());
-
     }
 
     @Test
-    public void test_S7_6_UndoPlayerDies(){
-        Tile PlayerTile = tileAt(1,1);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_5_UndoPlayerDownOnly() {
+        Tile Point1Tile = tileAt(0, 2);
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        // Move the Ghost first so the player doesn't die
+        getUI().getGame().moveGhost(theGhost(), Direction.UP);
+        getUI().getGame().moveGhost(theGhost(), Direction.LEFT);
+        getEngine().right();
+        getEngine().down();
+        getUI().undo();
+        assertEquals(GhostTile, getPlayer().getTile());
+    }
+
+    @Test
+    public void test_S7_5_UndoGhostDownOnly() {
+        Tile GhostTile = tileAt(2, 1);
+        getEngine().start();
+        getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
+        getUI().undo();
+        assertEquals(GhostTile, theGhost().getTile());
+    }
+
+    @Test
+    public void test_S7_6_UndoPlayerDies() {
+        Tile PlayerTile = tileAt(1, 1);
+        Tile GhostTile = tileAt(2, 1);
         getEngine().start();
         getEngine().right();
         getUI().undo();
@@ -117,9 +228,9 @@ public class UndoStoryTest extends MovePlayerStoryTest {
     }
 
     @Test
-    public void test_S7_7_UndoPlayerWins(){
-        Tile EmptyTile = tileAt(0,1);
-        Tile GhostTile = tileAt(2,1);
+    public void test_S7_7_UndoPlayerWins() {
+        Tile EmptyTile = tileAt(0, 1);
+        Tile GhostTile = tileAt(2, 1);
         getEngine().start();
         getUI().getGame().moveGhost(theGhost(), Direction.DOWN);
         getEngine().left(); // eat first food
