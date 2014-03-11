2096b70d6e80fc71d7ecc3c440d8a33520334051
Tony Li <tonyli.lives@gmail.com>
2014-03-11 01:13:24 -0700

    new game frames are saved on action

--- src/main/java/ca/ubc/jpacman/framework/model/UndoableGame.java
+++ src/main/java/ca/ubc/jpacman/framework/model/UndoableGame.java
@@ -18,10 +18,16 @@ public class UndoableGame extends Game {
     @Override
     public void movePlayer(Direction dir) {
         super.movePlayer(dir);
+        System.out.println("====================");
+        System.out.println("Player pushing new game frame into the frames stack");
+        System.out.println("====================");
+        frames.push(new GameFrame(this));
     }
 
     @Override
     public void moveGhost(Ghost theGhost, Direction dir) {
         super.moveGhost(theGhost, dir);
+        System.out.println("Ghost pushing new game frame into the frames stack");
+        frames.push(new GameFrame(this));
     }
 }
