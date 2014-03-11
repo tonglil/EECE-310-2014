291f592779b6e4afd44f6401bb1bea2b5f61b367
scolin22 <colin@bunnylaundering.com>
2014-03-11 02:56:51 -0700

    comments

--- src/main/java/ca/ubc/jpacman/framework/ui/UndoablePacmanInteraction.java
+++ src/main/java/ca/ubc/jpacman/framework/ui/UndoablePacmanInteraction.java
@@ -1,13 +1,16 @@
 package ca.ubc.jpacman.framework.ui;
 
+import org.jpacman.framework.model.IGameInteractor;
 import org.jpacman.framework.ui.PacmanInteraction;
 
 import ca.ubc.jpacman.framework.model.UndoableGame;
 
 public class UndoablePacmanInteraction extends PacmanInteraction {
-	void undo() {
+	public void undo() {
+		// We will need to update the state of the game without passing state
 		updateState();
-		UndoableGame undoableGame = (UndoableGame) getGame();
+		IGameInteractor igame = getGame();
+		UndoableGame undoableGame = (UndoableGame) igame;
 		undoableGame.undo();
 	}
 }
