d0f97d3a1ba490134f4f603ad973d6f60405e7ac
Tony Li <tonyli.lives@gmail.com>
2014-03-10 17:08:30 -0700

    finished 2.3: UndoableGameTest

--- src/main/java/ca/ubc/jpacman/framework/ui/UndoablePacman.java
+++ src/main/java/ca/ubc/jpacman/framework/ui/UndoablePacman.java
@@ -1,27 +1,9 @@
 package ca.ubc.jpacman.framework.ui;
 
-import org.jpacman.framework.factory.IGameFactory;
-import org.jpacman.framework.model.IGameInteractor;
 import org.jpacman.framework.ui.MainUI;
 
-import ca.ubc.jpacman.framework.factory.UndoableGameFactory;
-import ca.ubc.jpacman.framework.model.UndoableGame;
-
 public class UndoablePacman extends MainUI {
-	private IGameFactory fact;
-
-	public UndoablePacman() {
-		fact = new UndoableGameFactory();
-		withFactory(fact);
-	}
-
-	@Override
-	public IGameInteractor getGame() {
-		return (UndoableGame) super.getGame();
-	}
-
-	public void undo() {
-		// TODO Auto-generated method stub
-	}
-
+    public void undo() {
+        // TODO Auto-generated method stub
+    }
 }
