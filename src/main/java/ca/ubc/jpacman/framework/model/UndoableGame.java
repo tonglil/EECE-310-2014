dc5bc0dd5dde3980f09d7dfabe3360f8437c0fe8
Tony Li <tonyli.lives@gmail.com>
2014-03-11 22:19:59 -0700

    removed comments and styling

--- src/main/java/ca/ubc/jpacman/framework/model/UndoableGame.java
+++ src/main/java/ca/ubc/jpacman/framework/model/UndoableGame.java
@@ -5,14 +5,12 @@ import java.util.Deque;
 
 import org.jpacman.framework.model.Direction;
 import org.jpacman.framework.model.Game;
-import org.jpacman.framework.model.Ghost;
 import org.jpacman.framework.model.Player;
 
 public class UndoableGame extends Game {
     private Deque<GameFrame> frames = new ArrayDeque<GameFrame>();
 
     public void undo() {
-        // System.out.println("UndoableGame");
         if (frames.size() > 1) {
             GameFrame frame = this.frames.pop();
             frame.set(this);
@@ -21,8 +19,6 @@ public class UndoableGame extends Game {
             GameFrame frame = this.frames.peekFirst();
             frame.set(this);
             notifyViewers();
-        } else {
-            // System.out.println("Disable user from undoing");
         }
     }
 
@@ -30,24 +26,14 @@ public class UndoableGame extends Game {
     public void movePlayer(Direction dir) {
         frames.push(new GameFrame(this));
         super.movePlayer(dir);
-        // System.out.println("====================");
-        // System.out.println("Player pushing new game frame into the frames stack");
-        // System.out.println("====================");
     }
 
-    @Override
-    public void moveGhost(Ghost theGhost, Direction dir) {
-        super.moveGhost(theGhost, dir);
-        // System.out.println("Ghost pushing new game frame into the frames stack");
-        // frames.push(new GameFrame(this));
-    }
-
+    /*
+     *Save the first game frame
+     */
     @Override
     public void addPlayer(Player p) {
         super.addPlayer(p);
-        // System.out.println("====================");
-        // System.out.println("!!!!Push the first game frame into the frames stack");
-        // System.out.println("====================");
         frames.push(new GameFrame(this));
     }
 }
--- src/main/java/ca/ubc/jpacman/framework/ui/UndoableButtonPanel.java
+++ src/main/java/ca/ubc/jpacman/framework/ui/UndoableButtonPanel.java
@@ -8,30 +8,29 @@ import javax.swing.JButton;
 import org.jpacman.framework.ui.ButtonPanel;
 
 public class UndoableButtonPanel extends ButtonPanel {
+    private static final long serialVersionUID = -1231979178887149629L;
 
-	private static final long serialVersionUID = -1231979178887149629L;
+    @Override
+    public void initialize() {
+        JButton undoButton = createUndoButton();
+        super.initialize();
+        addButton(undoButton);
+    }
 
-	@Override
-	public void initialize() {
-		JButton undoButton = createUndoButton();
-		super.initialize();
-		addButton(undoButton);
-	}
+    protected JButton createUndoButton() {
+        JButton undoButton = new JButton("Undo");
+        undoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getPacmanInteractor().stop();
+                getPacmanInteractor().undo();
+            }
+        });
+        return undoButton;
+    }
 
-	protected JButton createUndoButton() {
-		JButton undoButton = new JButton("Undo");
-		undoButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				getPacmanInteractor().stop();
-				getPacmanInteractor().undo();
-			}
-		});
-		return undoButton;
-	}
-
-	@Override
-	public UndoablePacmanInteraction getPacmanInteractor() {
-		return (UndoablePacmanInteraction) super.getPacmanInteractor();
-	}
+    @Override
+    public UndoablePacmanInteraction getPacmanInteractor() {
+        return (UndoablePacmanInteraction) super.getPacmanInteractor();
+    }
 }
